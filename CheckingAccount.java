17ce61a237b1aa0c9fe8aaccba556786f2ca4df0
Estifanos Kebebew <65937750+Estifanos254@users.noreply.github.com>
2022-11-09 02:55:25 -0500

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,16 @@
+package lab6;
+
+public class CheckingAccount extends BankAccount{
+	
+	private static final double FEE = 0.12;
+	  
+	  public CheckingAccount(String name, double amount) {
+	    super(name, amount);
+	    super.setAccountNumber(super.getAccountNumber() + "-10");
+	  }
+	  
+	  public boolean withdraw(double amount) {   
+	    return super.withdraw(amount+FEE);
+	  }
+
+}
